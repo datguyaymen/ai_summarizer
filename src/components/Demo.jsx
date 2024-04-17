75f8e9125f7b227c5bf44b5f536d0c00e55ea13e
datguyaymen <the.aymen.benzarti@gmail.com>
2024-04-17 10:43:31 +0100

    finished fonctinality

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -2,11 +2,20 @@ import { useState, useEffect } from "react";
 import { copy, linkIcon, loader, tick } from "../assets";
 import { useLazyGetSummaryQuery } from "../services/article";
 const Demo = () => {
-  const [article, setarticle] = useState({
+  const [article, setArticle] = useState({
     url: "",
     summary: "",
   });
+  const [allArticles, setAllArticles] = useState([]);
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+  useEffect(() => {
+    const articleFromLocalStorage = JSON.parse(
+      localStorage.getItem("articles")
+    );
+    if (articleFromLocalStorage) {
+      setAllArticles(articleFromLocalStorage);
+    }
+  }, []);
   const handleSubmit = async (e) => {
     e.preventDefault();
     const { data } = await getSummary({
@@ -14,10 +23,13 @@ const Demo = () => {
     });
     if (data?.summary) {
       const newArticle = { ...article, summary: data.summary };
-      setarticle(newArticle);
-      console.log(newArticle);
+      const updatedAllArticles = [newArticle, ...allArticles];
+      setArticle(newArticle);
+      setAllArticles(updatedAllArticles);
+      localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
     }
   };
+
   return (
     <section className="w-full mt-16 max-w-xl">
       {/* Search */}
@@ -35,7 +47,7 @@ const Demo = () => {
             type="url"
             placeholder=" Enter a URL"
             value={article.url}
-            onChange={(e) => setarticle({ ...article, url: e.target.value })}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
             required
             className=" url_input peer"
           />
@@ -47,8 +59,53 @@ const Demo = () => {
           </button>
         </form>
         {/* Browse URL History */}
+        <div className=" flex flex-col gap-1 max-h-60 overflow-y-auto">
+          {allArticles.map((item, index) => (
+            <div
+              key={`link-${index}`}
+              onClick={() => setArticle(item)}
+              className=" link_card"
+            >
+              <div className=" copy_btn">
+                <img
+                  src={copy}
+                  alt="copy_icon"
+                  className="w-[40%] h-[40%] object-contain"
+                />
+              </div>
+              <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">
+                {item.url}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
       {/* Display Results */}
+      <div className=" my-10 max-w-full flex justify-center items-center">
+        {isFetching ? (
+          <img src={loader} alt="loader" className="w-20 h-20 object-contain" />
+        ) : error ? (
+          <p className=" font-inter font-bold text-black text-center">
+            Well, that was not supposed to happen ...
+            <span className=" font-satoshi font-normal text-gray-700">
+              {error?.data?.error}
+            </span>
+          </p>
+        ) : (
+          article.summary && (
+            <div className=" flex flex-col gap-3">
+              <h2 className=" font-satoshi font-bold text-gray-600 text-xl">
+                Article <span className=" blue_gradient">Summary</span>
+              </h2>
+              <div className="summary_box">
+                <p className=" font-inter font-medium text-sm text-gray-700">
+                  {article.summary}
+                </p>
+              </div>
+            </div>
+          )
+        )}
+      </div>
     </section>
   );
 };
